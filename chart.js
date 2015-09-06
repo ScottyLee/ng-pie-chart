18e71e1a5550bea1ccc3392805726379f1922b7a
Anthony Lapshin <anthony.lapshin@yahoo.com>
2015-09-06 22:04:41 +0300

    Add requestAnimationFrame function to window object

--- chart.js
+++ chart.js
@@ -21,9 +21,25 @@ function Chart (element_name, width, height) {
 
 	this.pices = [];
 	this.add_listeners();
+	window.requestAnimationFrame = ( function() {
 
-  	// console.log(requestAnimationFrame);
+    return window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element ) {
+
+      window.setTimeout( callback, 1000 / 60 );
+
+    };
+
+ 	})();
 }
+
+
+
+
+
 
 Chart.prototype.set_data = function(data) {
 	if (data == undefined || data == null){
